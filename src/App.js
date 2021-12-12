f6e77da688fb42608de365f1f53acce39b3e618f
soomroshoaib <soomroshoaib178@gmail.com>
2021-12-12 00:12:38 -0800

    update value

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-
+import React , {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
-  return (
+  
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+   fetch(`/.netlify/functions/Hello?name=from 12345Serverless Function`)
+   .then(res=> res.json())
+   .then(data => setData(data))
+   
+  }, []);
+
+
+  return ( 
     <div className="App">
       <header className="App-header">
-        <h1>hello world shoaib soomro sahab 1234567898 </h1>
+        
+          <h1>  {data.message}</h1>
+      
       </header>
     </div>
   );
